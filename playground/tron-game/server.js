e8716b88fe7469d766833fdb3b5a3d6c64a882a3
arthur mac <arthur.com50@gmail.com>
2025-06-13 16:41:28 -0300

    add `socket.io` lib and `server.js` file

---
+++ playground/tron-game/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import http from "http";
+import createGame from "./public/game.js";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const sockets = new Server(server);
+
+app.use(express.static("public"));
+
+const game = createGame();
+game.addPlayer({ playerId: "player1", playerX: 0, playerY: 0 });
+game.addPlayer({ playerId: "player2", playerX: 9, playerY: 1 });
+game.addFruit({ fruitId: "fruit1", fruitX: 3, fruitY: 3 });
+game.addFruit({ fruitId: "fruit2", fruitX: 6, fruitY: 6 });
+
+console.log(game.state);
+
+sockets.on("connect", (socket) => {
+  const playerId = socket.id;
+  console.log(`Player connected on Server with id: ${playerId}`);
+
+  socket.emit("setup", game.state);
+});
+
+server.listen(3000, () => {
+  console.log("> Server listening on port: 3000 ");
+});
