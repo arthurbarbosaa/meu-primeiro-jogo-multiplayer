b54d086269e92f2990cdfadd92da17fca46109a0
arthur mac <arthur.com50@gmail.com>
2025-11-11 13:54:01 -0300

    remove unused function

--- playground/tron-game/server.js
+++ playground/tron-game/server.js
@@ -89,26 +89,6 @@ sockets.on("connect", (socket) => {
     game.movePlayer(command);
   });
 
-  socket.on("try-collect-fruit", (data) => {
-    const { fruitId } = data;
-    const player = game.state.players[socket.id];
-    const fruit = game.state.fruits[fruitId];
-
-    if (player && fruit) {
-      // Se fruta ainda existe, concede ponto e remove
-      game.removeFruit({ fruitId });
-      player.score++;
-      sockets.emit("fruit-collected", {
-        playerId: socket.id,
-        fruitId,
-        score: player.score,
-      });
-    } else {
-      // Fruta já foi pega por outro
-      socket.emit("fruit-failed", { fruitId });
-    }
-  });
-
   // handle admin commands
   socket.on("admin-command", (cmd) => {
     if (socket.id !== adminId) {
